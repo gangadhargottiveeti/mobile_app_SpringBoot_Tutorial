2002cdeefaeedb00bc8f2187e867dc202853198f
gangadhar <gangadhar9398645347@gmail.com>
2024-04-24 11:39:48 +0530

    returning java object & set response status code

--- src/main/java/com/TW/mobile_app_ws/controller/UserController.java
+++ src/main/java/com/TW/mobile_app_ws/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.TW.mobile_app_ws.controller;
 
+import com.TW.mobile_app_ws.ui.model.response.UserRest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,13 +11,19 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping()
-    public String getUsers(@RequestParam("page") int page){
-        return "get user was called with page = "+page;
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "limit",defaultValue = "40") int limit){
+        return "get user was called with page = "+page+" and limit = "+limit;
     }
 
-    @GetMapping("/{userId}")
-    public String getUser(@PathVariable String userId){
-        return "get user was called with userId : "+userId ;
+
+    @GetMapping(value = "/{userId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<UserRest> getUser(@PathVariable String userId){
+        UserRest returnValue = new UserRest();
+        returnValue.setFirstName("Gangadhar");
+        returnValue.setLastName("Gottiveeti");
+        returnValue.setEmail("gottiveetigangadhar@gmail.com");
+        returnValue.setUserId(2);
+        return new ResponseEntity<UserRest>(returnValue,HttpStatus.OK);
     }
 
     @PostMapping()
